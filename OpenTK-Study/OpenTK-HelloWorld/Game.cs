08d931e301e1f861151489cc94d57bd36d10a826
Wesley Santos <wesleysotnas64@gmail.com>
2024-09-02 21:42:06 -0300

    Instalando OpenTK e Criando primeira Janela.

---
+++ OpenTK-Study/OpenTK-HelloWorld/Game.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace OpenTK_HelloWorld
+{
+    public class Game : GameWindow
+    {
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+
+            if(KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+
+            base.OnUpdateFrame(e);
+        }
+    }
+}
---
+++ OpenTK-Study/OpenTK-HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK_HelloWorld;
+
+var nativeWindowSettings = new NativeWindowSettings()
+{
+    ClientSize = new Vector2i(800, 600),
+    Title = "LearnOpenTK - Creating a Window",
+};
+
+using(var game = new Game(GameWindowSettings.Default, nativeWindowSettings))
+{
+    game.Run();
+}
